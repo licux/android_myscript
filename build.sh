3874aaf8d6a99dddb2c7774dd4dfd643bc973365
masaki <you@example.com>
2019-12-14 08:45:35 +0900

    Boot from Snapshot image

---
+++ build.sh
@@ -0,0 +1,9 @@
+echo "remove old file..."
+rm out/target/product/x86emu/x86emu_tmp/ -R
+echo "make image..."
+#make -j8
+echo "make initrd.img..."
+make qcow2_img USE_SQUASHFS=0
+echo "copy images..."
+./copy_image.sh 
+
--- emulator.sh
+++ emulator.sh
@@ -1,8 +1,2 @@
-#source build/envsetup.sh
-#lunch x86emu-eng
-#IMG_ROOT=$OUT
 IMG_ROOT=out/target/product/x86emu
-~/Android/Sdk/emulator/emulator @x86emu -no-snapshot -verbose -show-kernel -shell -system $IMG_ROOT/system.img -ramdisk $IMG_ROOT/initrd.img -initdata $IMG_ROOT/userdata.img -kernel $IMG_ROOT/kernel -qemu -append "qemu=1 clocksource=pit androidboot.selinux=permissive DEBUG=2 console=ttyS0,11520 androidboot.hardware=ranchu qemu.gles=1 android.qemud=1 root=/dev/sda SRC=x86emu" -hda $IMG_ROOT/x86emu.img
-
-
-
+~/Android/Sdk/emulator/emulator @x86emu -verbose -no-snapshot -show-kernel -shell -ramdisk $IMG_ROOT/initrd.img -initdata $IMG_ROOT/userdata.img -kernel $IMG_ROOT/kernel -qemu -append "qemu=1 clocksource=pit androidboot.selinux=permissive DEBUG=2 console=ttyS0,11520 androidboot.hardware=ranchu qemu.gles=1 android.qemud=1 root=/dev/sda SRC=x86emu " -hda $IMG_ROOT/x86emu.img

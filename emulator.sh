8625ceb3a7d8745c45b11791ecd675171e26b1fd
masaki <you@example.com>
2020-01-19 08:43:07 +0900

    Passthrough USB device

--- emulator.sh
+++ emulator.sh
@@ -1,6 +1,5 @@
 IMG_ROOT=out/target/product/x86emu
 ~/Android/Sdk/emulator/emulator @x86emu \
 -verbose -no-snapshot -show-kernel -shell -ramdisk $IMG_ROOT/initrd.img -data $IMG_ROOT/userdata.img -initdata $IMG_ROOT/userdata.img -kernel $IMG_ROOT/kernel \
--qemu -usb -device usb-ehci,id=ehci -append "qemu=1 clocksource=pit androidboot.selinux=permissive DEBUG=2 console=ttyS0,11520 androidboot.hardware=ranchu qemu.gles=1 android.qemud=1 root=/dev/sda SRC=x86emu " \
+-qemu -device usb-ehci,id=ehci -usb -device usb-host,vendorid=0x0411,productid=0x01dd -append "qemu=1 clocksource=pit androidboot.selinux=permissive DEBUG=2 console=ttyS0,11520 androidboot.hardware=ranchu qemu.gles=1 android.qemud=1 root=/dev/sda SRC=x86emu " \
 -hda $IMG_ROOT/x86emu.img
-
